23ef327b17d8f71a9c68fabd980892f40a76270e
Dario Feghali <dario.feghali@epitech.eu>
2021-04-29 14:55:16 +0200

    [EDIT] cleanup tests for plnm_scale

--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -11,26 +11,8 @@ Test(plnm_scale, quadraticValue1)
 	plnm_init_quadratic(&quad, 2, 3, 5);
 	plnm_scale(&quad, 2);
 	cr_expect(quad[1] == 4);
-	free(quad);
-}
-
-Test(plnm_scale, quadraticValue2)
-{
-	polynomial_t quad = NULL;
-
-	plnm_init_quadratic(&quad, 2, 3, 5);
-	plnm_scale(&quad, 2);
-	cr_expect(quad[2] == 6);
-	free(quad);
-}
-
-Test(plnm_scale, quadraticValue3)
-{
-	polynomial_t quad = NULL;
-
-	plnm_init_quadratic(&quad, 2, 3, 5);
-	plnm_scale(&quad, 2);
-	cr_expect(quad[3] == 10);
+        cr_expect(quad[2] == 6);
+        cr_expect(quad[3] == 10);
 	free(quad);
 }
 
